9151f9c2a6fa5bd62bee3c5fe65dea7ffeec5f0d
tusharkharakwal-maker <tushar@example.com>
2026-07-31 19:34:46 +0530

    Use dragListener to properly disable dynamic swiping in Framer Motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,7 +56,8 @@ export default function StoryContainer() {
       {/* Container for swipeable scenes */}
       <motion.div
         className="flex-1 relative w-full h-full outline-none"
-        drag={[4, 5, 6].includes(sceneIndex) ? false : "x"}
+        drag="x"
+        dragListener={![4, 5, 6].includes(sceneIndex)}
         dragConstraints={{ left: 0, right: 0 }}
         dragElastic={1}
         onDragEnd={(e, { offset, velocity }) => {
